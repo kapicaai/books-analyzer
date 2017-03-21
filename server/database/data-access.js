95a09ed190d7684c193f164c0f82bb37e9217be9
Olga Yaroshchuk <yrox152@gmail.com>
2017-03-22 01:55:10 +0300

    replaced to dal

--- server/database/data-access.js
+++
@@ -1,8 +0,0 @@
-
-//TODO: Create some data access layer
-var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/test');
-var db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
