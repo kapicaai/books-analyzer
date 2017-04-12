6a5c935ba90fa4b8a56da6535fb27fc0bae42f74
kapicaai <marg_devil@outlook.com>
2017-04-12 10:34:56 +0300

    text analysis
    # Conflicts:
    # 	backend/package.json

--- backend/server/domain/parser.js
+++ backend/server/domain/parser.js
@@ -1,40 +1,64 @@
 const splitter = require('sentence-splitter');
-const tm = require( 'text-miner' );
+const tm = require('text-miner');
 
-function getNormalizedCorpus(text){
+var normalizedCorpus;
+var sentences;
+
+function getNormalizedCorpus(text, stopWords) {
   var corpus = tm.Corpus(text);
   corpus.toLower().removeDigits().removeInterpunctuation().removeNewlines();
   corpus.removeWords(tm.STOPWORDS.EN)
   return corpus;
 }
 
-function getUniqueWordsList(text){
-  var corpus = getNormalizedCorpus(text);
-  return tm.Terms(corpus).vocabulary;
-}
-
-function countWords(text){
-  var corpus = getNormalizedCorpus(text);
-  var arr = tm.Terms(corpus).dtm[0];
-  var result = arr.reduce(function(sum, current) {
-      return sum + current;
-    }, 0);
-  return result;
-}
-
-function getVocabulary(text){
-  var corpus = getNormalizedCorpus(text);
-  var terms = tm.Terms(corpus);
-  var result = [];
-  for (var i = 0; i < terms.vocabulary.length; i++) {
-    result.push({word: terms.vocabulary[i], count: terms.dtm[0][i]});
-  }
-  return result;
-}
-
-function getSentences(text){
+function getSentences(text) {
   var allTokens = splitter.split(text);
-  var result = allTokens.filter(function(token) {
-  return token.type === splitter.Syntax.Sentence;});
+  var result = allTokens.filter(function (token) {
+  return token.type === splitter.Syntax.Sentence;
+});
   return result;
 }
+
+function PARSER(text) {
+  normalizedCorpus = getNormalizedCorpus(text);
+  sentences = getSentences(text);
+
+  return {
+    getUniqueWordsList() {
+      return tm.Terms(normalizedCorpus).vocabulary;
+    },
+
+    countWords() {
+      var arr = tm.Terms(normalizedCorpus).dtm[0];
+      var result = arr.reduce(function (sum, current) {
+          return sum + current;
+        }, 0);
+      return result;
+    },
+
+    getVocabulary() {
+      var terms = tm.Terms(normalizedCorpus);
+      var result = [];
+      for (var i = 0; i < terms.vocabulary.length; i++) {
+        result.push({word: terms.vocabulary[i], count: terms.dtm[0][i]});
+      }
+      return result;
+    },
+
+    getNumberOfSentences() {
+      return sentences.length;
+    },
+
+//TODO correct word splitting
+    getAverageSentLength() {
+      var allWords = 0;
+      for (let i = 0; i < sentences.length; i++) {
+        allWords += sentences[i].value.split(/\s+/).length;
+      }
+      return allWords / sentences.length;
+    }
+
+  }
+}
+
+module.exports = PARSER;
