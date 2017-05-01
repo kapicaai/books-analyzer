318dc608a484c56762597955818d2fb76397ac93
kapicaai <marg_devil@outlook.com>
2017-05-01 13:23:46 +0300

    Auto stash for cherrypick of "simplified getAll"

--- backend/server/routes/api/book.js
+++ backend/server/routes/api/book.js
@@ -14,28 +14,6 @@ function analyze(req, res) {
   req.body.analysis = analyzer(req.body.text);
 }
 
-function saveFile(req, res) {
-  const textStorage = FileStorage(textPath);
-  try {
-    const id = textStorage.addFile(req.body.text);
-    req.body.text = id;
-  } catch (err) {
-    res.code = 500;
-    res.end();
-  }
-}
-
-function saveImage(req, res) {
-  const imgStorage = FileStorage(imgPath);
-  try {
-    const id = imgStorage.addFile(req.body.image);
-    req.body.image = id;
-  } catch (err) {
-    res.code = 500;
-    res.end();
-  }
-}
-
 router.get('/simplified', CRUD.getAllSimplified);
 
 router.get('/', CRUD.getAll);
